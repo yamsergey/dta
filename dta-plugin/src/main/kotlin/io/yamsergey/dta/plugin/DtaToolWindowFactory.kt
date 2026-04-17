252247d7acb0d06e1ad43c50e9f806c946520c3c
Sergey Yamshchikov <yamsegey@gmail.com>
2026-04-17 13:05:44 +0200

    Plugin icon: load via IconLoader for dark theme support

--- dta-plugin/src/main/kotlin/io/yamsergey/dta/plugin/DtaToolWindowFactory.kt
+++ dta-plugin/src/main/kotlin/io/yamsergey/dta/plugin/DtaToolWindowFactory.kt
@@ -1,8 +1,8 @@
 package io.yamsergey.dta.plugin
 
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
@@ -13,12 +13,13 @@ import com.intellij.ui.content.ContentFactory
  */
 class DtaToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        toolWindow.setIcon(IconLoader.getIcon("/icons/dta-13.png", DtaToolWindowFactory::class.java))
+
         val panel = DtaToolWindowPanel()
         val content = ContentFactory.getInstance().createContent(panel, "Inspector", false)
         content.setDisposer(panel)
         toolWindow.contentManager.addContent(content)
 
-        // Ensure panel is disposed when the tool window is disposed
         Disposer.register(toolWindow.disposable, panel)
     }
 }
