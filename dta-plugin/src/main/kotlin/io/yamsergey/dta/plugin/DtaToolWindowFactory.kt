6acb67b779066b4a5b6fae78ee19d16ba2e26790
Sergey Yamshchikov <yamsegey@gmail.com>
2026-04-23 07:49:10 +0200

    Plugin icon: visor droid SVG with bold strokes + dark variant

--- dta-plugin/src/main/kotlin/io/yamsergey/dta/plugin/DtaToolWindowFactory.kt
+++ dta-plugin/src/main/kotlin/io/yamsergey/dta/plugin/DtaToolWindowFactory.kt
@@ -2,7 +2,6 @@ package io.yamsergey.dta.plugin
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
@@ -13,8 +12,6 @@ import com.intellij.ui.content.ContentFactory
  */
 class DtaToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        toolWindow.setIcon(IconLoader.getIcon("/icons/dta-13.png", DtaToolWindowFactory::class.java))
-
         val panel = DtaToolWindowPanel()
         val content = ContentFactory.getInstance().createContent(panel, "Inspector", false)
         content.setDisposer(panel)
